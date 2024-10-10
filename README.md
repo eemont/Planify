20f26fb03f989cc156e314af7729db4c6f8a51de
sxlpz <sharonlopez723@gmail.com>
2024-10-09 17:56:02 -0700

    firebase files working

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Planify
+
+## __Student names :__
+
+<li>Emmanuel Montoya</li>
+
+<li>Sharon Lopez</li>
+
+<li>Eric Nakayama</li>
+
+<li>Andrew Davalos</li>
+
+## Technologies used:
+
+<li>Flutter</li>
+<li>VSCode</li>
+<li>XCode</li>
+<li>Android Studio</li>
+
+
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Planify',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 24, 185, 129)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Planify Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/Planify_Logo_Black copy.png',
+              width: 200,
+              height: 200,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Log In'),
+            ),
+            const SizedBox(height: 10), // Spacing between buttons
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignUpPage()),
+                );
+              },
+              child: const Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// LoginPage widget
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Log In')),
+      body: Center(
+        child: const Text('This is the Log In page'),
+      ),
+    );
+  }
+}
+
+// SignUpPage widget
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Sign Up')),
+      body: Center(
+        child: const Text('This is the Sign Up page'),
+      ),
+    );
+  }
+}
